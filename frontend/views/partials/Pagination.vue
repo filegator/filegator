2c757eea5f6e07800079c150a213cfba96e6ee2d
Milos Stojanovic <alcalbg@gmail.com>
2019-06-16 19:14:17 +0200

    per page cleanup

--- frontend/views/partials/Pagination.vue
+++ frontend/views/partials/Pagination.vue
@@ -2,7 +2,6 @@
   <div>
       <b-select :value="perpage" @input="$emit('selected', $event)" size="is-small">
         <option value="">{{ lang('No pagination') }}</option>
-        <option value="3">{{ lang('Per page', 3) }}</option>
         <option value="5">{{ lang('Per page', 5) }}</option>
         <option value="10">{{ lang('Per page', 10) }}</option>
         <option value="15">{{ lang('Per page', 15) }}</option>
