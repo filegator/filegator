9255d7dd8a2f63fd414bbac5fdaca977d03dea20
Milos Stojanovic <alcalbg@gmail.com>
2019-06-16 19:18:09 +0200

    version update RC2

--- dist/index.php
+++ dist/index.php
@@ -35,7 +35,7 @@ if (! defined('APP_PUBLIC_PATH')) {
 }
 
 define('APP_PUBLIC_DIR', __DIR__);
-define('APP_VERSION', '7.0.0');
+define('APP_VERSION', '7.0.0-RC2');
 
 use Filegator\App;
 use Filegator\Config\Config;
