b08d105cef6154e476d086a54eca89d5e8f09d53
Milos Stojanovic <alcalbg@gmail.com>
2019-06-25 15:26:36 +0200

    vue eslinter config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    // add more generic rulesets here, such as:
+    'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    'vue/no-unused-vars': 'error',
+    'vue/require-prop-types': 0,
+    'vue/require-default-prop': 0
+  }
+}
