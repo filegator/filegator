0af6d13315c633975077349511dde75b8547253c
Milos Stojanovic <alcalbg@gmail.com>
2019-06-25 16:44:27 +0200

    js lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,7 @@ module.exports = {
   rules: {
     // override/add rules settings here, such as:
     'vue/no-unused-vars': 'error',
+    'vue/require-prop-types': 0,
+    'vue/require-default-prop': 0
   }
 }
