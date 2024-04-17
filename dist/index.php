90557f0f096e26fb39ead24d4c2c8d41ae2f4211
Milos Stojanovic <alcalbg@gmail.com>
2024-04-17 17:30:05 +0200

    version bump and changelog upd - 7.10.0

--- dist/index.php
+++ dist/index.php
@@ -36,7 +36,7 @@ if (! defined('APP_PUBLIC_PATH')) {
 }
 
 define('APP_PUBLIC_DIR', __DIR__);
-define('APP_VERSION', '7.9.3');
+define('APP_VERSION', '7.10.0');
 
 use Filegator\App;
 use Filegator\Config\Config;
