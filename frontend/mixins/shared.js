18918fc2bdfd54b9298171ed12ab208cef205455
alkl58 <alkl58@protonmail.com>
2024-11-06 16:51:20 +0100

    Update buefy dialog, notification, toast

--- frontend/mixins/shared.js
+++ frontend/mixins/shared.js
@@ -116,14 +116,14 @@ const funcs = {
         .then((user) => {
           if (user.username !== store.state.user.username) {
             this.$store.commit('destroyUser', user)
-            this.$toast.open({
+            this.$buefy.toast.open({
               message: this.lang('Please log in'),
               type: 'is-danger',
             })
           }
         })
         .catch(() => {
-          this.$toast.open({
+          this.$buefy.toast.open({
             message: this.lang('Please log in'),
             type: 'is-danger',
           })
@@ -133,14 +133,14 @@ const funcs = {
       this.checkUser()
 
       if (typeof error == 'string') {
-        this.$toast.open({
+        this.$buefy.toast.open({
           message: this.lang(error),
           type: 'is-danger',
           duration: 5000,
         })
         return
       } else if (error && error.response && error.response.data && error.response.data.data) {
-        this.$toast.open({
+        this.$buefy.toast.open({
           message: this.lang(error.response.data.data),
           type: 'is-danger',
           duration: 5000,
@@ -148,7 +148,7 @@ const funcs = {
         return
       }
 
-      this.$toast.open({
+      this.$buefy.toast.open({
         message: this.lang('Unknown error'),
         type: 'is-danger',
         duration: 5000,
