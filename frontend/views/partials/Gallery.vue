1d50a018fe85164d5e87ed1f6432bdc8751f0433
Robert-Jan de Dreu <160743+rjd22@users.noreply.github.com>
2025-02-22 17:10:42 +0100

    Fix lazy loading images all being loaded when opening gallery
    Because the lazy loaded image didn't have a limited height. Many
    images would load anyway because they all fit in the parent box.
    Giving them a minimal height with object-cover keeps them looking
    good while limiting the amount that is lazy loaded.

--- frontend/views/partials/Gallery.vue
+++ frontend/views/partials/Gallery.vue
@@ -10,7 +10,7 @@
           <div v-if="images.length > 1" class="column is-one-fifth sidebox">
             <ul>
               <li v-for="(image, index) in images" :key="index">
-                <img v-lazy="imageSrc(image.path)" @click="currentItem = image">
+                <img v-lazy="imageSrc(image.path)" @click="currentItem = image" class="image-lazy">
               </li>
             </ul>
           </div>
