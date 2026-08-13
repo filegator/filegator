ca39d0abd26a6f40f3f15523b6da853f631c91f7
alcalbg <alcalbg@gmail.com>
2026-08-13 18:13:27 +0200

    version bump

--- dist/index.php
+++ dist/index.php
@@ -36,7 +36,7 @@ if (! defined('APP_PUBLIC_PATH')) {
 }
 
 define('APP_PUBLIC_DIR', __DIR__);
-define('APP_VERSION', '7.15.0');
+define('APP_VERSION', '7.15.1');
 
 use Filegator\App;
 use Filegator\Config\Config;
